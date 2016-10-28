e8aa6cd376d8977113198c2d603b5c089fe2f537
Georgiy Frolov <gfrolov@sensics.com>
2016-10-28 12:50:39 -0400

    add settings page and controller

---
+++ src/SensicsTray/wwwroot/app/settings/settingsController.ts
@@ -0,0 +1,45 @@
+﻿/// OSVR-Config
+///
+/// <copyright>
+/// Copyright 2016 Sensics, Inc.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///     http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+/// </copyright>
+///
+module app.settings {
+    class SettingsController {
+
+        saveSettings() {
+            this.$http.post("/api/savesettings", {}).then(
+                success => {
+                    console.log("savesettings call succeeded.");
+                },
+                failure => {
+                    console.log("savesettings call failed.");
+                });
+        }
+
+        static $inject = ["$http"];
+        constructor(private $http: ng.IHttpService) { }
+    }
+
+    angular.module("app.settings", ["ui.router"])
+        .config(["$stateProvider", ($stateProvider: angular.ui.IStateProvider) => {
+            $stateProvider.state("settings", {
+                url: "/settings",
+                templateUrl: "app/settings/settings.html",
+                controller: "app.settings.SettingsController as vm"
+            });
+        }])
+        .controller("app.settings.SettingsController", SettingsController);
+}
