bb6cd6429d046495f803f62e3752b6fcae47d0f7
JeroMiya <bell.jeremy@gmail.com>
2016-11-04 17:48:10 -0400

    Filled in some requests in OSVRServerService and TrackerViewerService.

--- src/SensicsTray/wwwroot/app/services/tracker-viewer.service.ts
+++ src/SensicsTray/wwwroot/app/services/tracker-viewer.service.ts
@@ -1,8 +1,14 @@
 ﻿import { Injectable } from '@angular/core';
+import { Http } from "@angular/http"
+import 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
+
 import { UserNotificationsService } from './user-notifications.service';
 
 @Injectable()
 export class TrackerViewerService {
+    private startTrackerViewerURL = "/api/startTrackerViewer";
+
     constructor(private userNotifications: UserNotificationsService) { }
 
     startTrackerViewer(paths: string[] | string): Promise<void> {
