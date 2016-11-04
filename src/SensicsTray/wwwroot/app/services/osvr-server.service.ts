d42cb476e17fd1bfb47269059b000a6ac124d138
JeroMiya <bell.jeremy@gmail.com>
2016-11-04 13:40:05 -0400

    Updated gulp-typescript to 3.0 and the typescript compiler to 2.0.7. Fixes compiler issues with rxjs's typescript declarations.

--- src/SensicsTray/wwwroot/app/services/osvr-server.service.ts
+++ src/SensicsTray/wwwroot/app/services/osvr-server.service.ts
@@ -1,12 +1,24 @@
-﻿import { Injectable } from "@angular/core"
+﻿import { Injectable } from "@angular/core";
+import { Http } from '@angular/http';
 import { UserNotificationsService } from './user-notifications.service';
+
+import 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class OSVRServerService {
-    constructor(private userNotifications: UserNotificationsService) { }
+    private startServerURL = "/api/startserver";
+    private stopServerURL = "/api/stopserver";
+
+    constructor(
+        private http: Http,
+        private userNotifications: UserNotificationsService) { }
 
     startServer(): Promise<void> {
-        return this.userNotifications.wrapPromise(Promise.resolve(),
+        var ret = this.http.get(this.startServerURL)
+            .toPromise() as any;
+
+        return this.userNotifications.wrapPromise<void>(ret,
             "OSVRServerService.startServer() - success!");
     }
 
