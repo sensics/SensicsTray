257d074125a8f375765e7d0aae356e9f458af98b
Georgiy Frolov <gfrolov@sensics.com>
2016-11-06 17:24:22 -0500

    add device interface, additional modifications to device component

--- src/SensicsTray/wwwroot/app/components/devices/devices.component.ts
+++ src/SensicsTray/wwwroot/app/components/devices/devices.component.ts
@@ -1,14 +1,41 @@
-﻿import { Component } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
+import { Response } from '@angular/http';
 import { DevicesService } from '../../services/devices.service';
+
+export interface IOSVRDevice {
+    id: number;
+    path: string;
+    vid: string;
+    pid: string;
+    friendlyName: string;
+    modelName: string;
+    vendorName: string;
+    version: string;
+    deviceType: string;
+    enabled: boolean;
+}
 
 @Component({
     moduleId: module.id,
     selector: 'ts-devices',
     templateUrl: 'devices.html'
 })
-export class DevicesComponent {
-    constructor(private devices: DevicesService) { }
-    startDeviceMonitor() {
-        this.devices.startDeviceMonitor();
+export class DevicesComponent implements OnInit {
+    constructor(private devService: DevicesService) { }
+    //startDeviceMonitor() {
+    //    this.devService.startDeviceMonitor();
+    //}
+
+    private usbDevices: IOSVRDevice[];
+
+    ngOnInit() {
+        this.devService.getDevices().subscribe(
+            (response: Response) => {
+                console.log("Received devices");
+                this.usbDevices = response.json();
+            },
+            (error: any) => {
+                console.log("Got an error");
+            });
     }
 }
