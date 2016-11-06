82df446f4f3486fe29558307db808449583fea98
Georgiy Frolov <gfrolov@sensics.com>
2016-11-06 17:24:59 -0500

    add observable to device service

--- src/SensicsTray/wwwroot/app/services/devices.service.ts
+++ src/SensicsTray/wwwroot/app/services/devices.service.ts
@@ -1,21 +1,26 @@
 ﻿import { Injectable } from "@angular/core";
+import { Response } from '@angular/http';
 import { Http } from '@angular/http';
-import 'rxjs/Rx';
-import { Observable } from 'rxjs/Observable';
-
+import { Observable } from "rxjs/Observable";
 import { UserNotificationsService } from './user-notifications.service';
+
+import 'rxjs/Rx';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class DevicesService {
-    private startDeviceMonitorUrl = "/api/startdevmonitor";
+    private getUsbDevicesUrl = "/api/getusbdevices";
     constructor(
         private http: Http,
         private userNotifications: UserNotificationsService) { }
-
-    startDeviceMonitor(): Observable<any> {
-        var observable = this.http.post(this.startDeviceMonitorUrl, {}).map(
-            response => response.json() as any);
-        return this.userNotifications.wrapObservable(observable,
-            "Device monitor started!", "Could not start device monitor.");
+    
+    //startDeviceMonitor(): Promise<any> {
+    //    var promise = this.http.post(this.startDeviceMonitorUrl, {}).toPromise().then(
+    //        response => response.json() as any);
+    //    return this.userNotifications.wrapPromise(promise, "Device monitor started!");
+    //}
+    getDevices(): Observable<Response> {
+        console.log("DeviceService : GetDevices");
+        return this.http.get(this.getUsbDevicesUrl);
     }
 }
