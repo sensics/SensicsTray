f501417f5ca40ec9d32f47c9304f59c693535bc5
JeroMiya <bell.jeremy@gmail.com>
2016-11-08 12:11:13 -0500

    Updates to the SensicsTray project to reference osvr-config class libraries.

--- src/SensicsTray/wwwroot/app/services/tracker-viewer.service.ts
+++ src/SensicsTray/wwwroot/app/services/tracker-viewer.service.ts
@@ -1,5 +1,5 @@
 ﻿import { Injectable } from '@angular/core';
-import { Http } from "@angular/http"
+import { Http, URLSearchParams } from "@angular/http"
 import 'rxjs/Rx';
 import 'rxjs/add/operator/toPromise';
 
@@ -9,16 +9,24 @@ import { UserNotificationsService } from './user-notifications.service';
 export class TrackerViewerService {
     private startTrackerViewerURL = "/api/startTrackerViewer";
 
-    constructor(private userNotifications: UserNotificationsService) { }
+    constructor(
+        private userNotifications: UserNotificationsService,
+        private http: Http)
+    { }
 
-    startTrackerViewer(paths: string[] | string): Promise<void> {
+    startTrackerViewer(paths: string[] | string): Promise<any> {
         if (typeof paths === 'undefined' || paths === null) {
             paths = [];
         }
         if (typeof paths === 'string') {
             paths = (<string>paths).split(" ");
         }
-        return this.userNotifications.wrapPromise(Promise.resolve(),
+
+        var promise = this.http.post(
+            this.startTrackerViewerURL + `?paths=${paths.join(',')}`, {}).toPromise()
+            .then(response => response.json() as any);
+
+        return this.userNotifications.wrapPromise(promise,
             "TrackerViewerService.startTrackerViewer() - success!");
     }
 }
