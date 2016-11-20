fd50a2c59a3a68d17dc29475521c5033dc38835b
Georgiy Frolov <gfrolov@sensics.com>
2016-11-20 17:37:45 -0500

    add GetUSBEvent and GetDeviceList functions to DeviceController, and P/Invoke USBDetectionLib

--- src/SensicsTray/Controllers/DevicesController.cs
+++ src/SensicsTray/Controllers/DevicesController.cs
@@ -1,38 +1,96 @@
 using System;
 using System.Collections.Generic;
-using System.Management;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 using TrayApp.Devices;
 namespace TrayApp.Controllers
 {
-    
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct OSVR_USBDeviceDescriptor
+    {
+        public UInt16 productID;
+        public UInt16 vendorID;
+    }
+
+
     [Route("api/[controller]")]
     public class DevicesController : Controller
     {
-              
-        // GET: api/getusbdevices
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern IntPtr osvrUSBDetectionMonitorInit();
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void osvrUSBDetectionStartMonitor(IntPtr monitor);
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern sbyte osvrUSBDetectionStopMonitor(IntPtr monitor);
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int osvrUSBDetectionMonitorUpdate(IntPtr monitor, ref OSVR_USBDeviceDescriptor dev);
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void osvrUSBDetectionMonitorShutdown(IntPtr monitor);
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint osvrUSBDetectionGetDeviceList(IntPtr monitor, ref OSVR_USBDeviceDescriptor[] list);
+
+        [DllImport("osvrUSBDetection", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void osvrUSBDetectionFreeDeviceList(OSVR_USBDeviceDescriptor[] list, uint numDevices);
+
+        // GET: api/GetUSBDevices
         [HttpGet]
-        public IEnumerable<OSVRDevice> Get()
+        public IEnumerable<USBDevice> GetUSBDevices()
         {
-            List<OSVRDevice> devices = new List<OSVRDevice>();
-            ManagementObjectCollection collection;
-            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
-            collection = searcher.Get();      
+            Console.WriteLine("GetUSBDevices\n");
+            List<USBDevice> devices = new List<USBDevice>();
 
-            foreach (var device in collection)
-            {
-                devices.Add(new OSVRDevice(
-                (string)device.GetPropertyValue("DeviceID"),
-                (string)device.GetPropertyValue("PNPDeviceID"),
-                (string)device.GetPropertyValue("Description"),
-                (string)device.GetPropertyValue("Name"),
-                (string)device.GetPropertyValue("Manufacturer")
-                ));
+            IntPtr usbMonitor = osvrUSBDetectionMonitorInit();
+            OSVR_USBDeviceDescriptor[] devList = null;
+            uint numDevices = osvrUSBDetectionGetDeviceList(usbMonitor, ref devList);
+            Console.WriteLine("GetUSBDevices : Got {0} number of conncted devices\n", numDevices);
+            for (int i = 0; i < numDevices; i++) {
+                USBDevice dev = new USBDevice(devList[i].vendorID, devList[i].productID);
+                Console.WriteLine("GetUSBDevices : Adding devices with VID:{0} and PID:{0}\n", dev.VendorID, dev.ProductID);
+                devices.Add(dev);
             }
-
-            collection.Dispose();
+            osvrUSBDetectionMonitorShutdown(usbMonitor);
+            Console.WriteLine("GetUSBDevices : Monitor shutdown, returning");
             return devices;
         }
+
+        // GET: api/GetUSBEvent
+        [HttpGet]
+        public USBEvent GetUSBEvent()
+        {
+            Console.WriteLine("GetUSBEvent\n");
+            IntPtr usbMonitor = osvrUSBDetectionMonitorInit();
+            OSVR_USBDeviceDescriptor devDesc = new OSVR_USBDeviceDescriptor();
+            EventCode eventCode = (EventCode) osvrUSBDetectionMonitorUpdate(usbMonitor, ref devDesc);
+            USBDevice dev = new USBDevice(devDesc.vendorID, devDesc.productID);
+            USBEvent deviceEvent = new USBEvent(eventCode, dev);
+
+            if (eventCode == EventCode.USB_DEVICE_NO_STATUS_CHANGE)
+            {
+                Console.WriteLine("GetUSBEvent: No status change\n");
+            }
+            else if(eventCode == EventCode.USB_DEVICE_ADDED)
+            {
+                Console.WriteLine("GetUSBEvent: Device added\n");
+            }
+            else if(eventCode == EventCode.USB_DEVICE_REMOVED) 
+            {
+                Console.WriteLine("GetUSBEvent: Device removed\n");
+            }
+            else {
+                Console.WriteLine("Got an event code that is not recognized\n");
+            }
+            osvrUSBDetectionMonitorShutdown(usbMonitor);
+            Console.WriteLine("GetUSBDevices : Monitor shutdown, returning");
+            return deviceEvent;
+        }
+        
     }
-    
+
 }
