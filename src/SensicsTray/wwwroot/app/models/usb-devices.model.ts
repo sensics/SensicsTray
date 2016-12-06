36d78448b8da4ebbcf7090d00c4a68811e75f386
JeroMiya <bell.jeremy@gmail.com>
2016-12-06 17:55:33 -0500

    Fixed the devices service - now using the correct URLs for the devices backend service. Also fixed the frontend devices list to display the vendorID/productID. The USB event polling now causes the device list to refresh, thus the list is now a 'live' list.

---
+++ src/SensicsTray/wwwroot/app/models/usb-devices.model.ts
@@ -0,0 +1,15 @@
+﻿export interface IUSBDevice {
+    vendorID: string;
+    productID: string;
+    friendlyName: string;
+    modelName: string;
+    vendorName: string;
+    firmwareVersion: string;
+    deviceType: string;
+    enabled: boolean;
+}
+
+export interface IUSBEvent {
+    statusCode: string;
+    deviceDescriptor: IUSBDevice;
+}
